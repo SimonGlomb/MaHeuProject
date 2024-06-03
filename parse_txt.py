0ab8f6787b587241a5f687a83955bd36f7f02eef
Simon Glomb <simon.glomb@rwth-aachen.de>
2024-06-03 18:08:30 +0200

    improve commenting

--- parse_txt.py
+++ parse_txt.py
@@ -4,8 +4,10 @@ import re
 def parse_file(file_path):
     data = {}
     columns = {}
-    sections = set()
-    current_section = None
+    # safe all types and use the keyword of the first column to determine in which dataframe
+    # necessary as sometimes two types and their columns are explained right after each other
+    types = set()
+    current_type = None
 
     with open(file_path, 'r') as file:
         for line in file:
@@ -14,15 +16,15 @@ def parse_file(file_path):
 
                 match = re.match(r'#(\w+)-(.*)', line)
                 if match:
-                    current_section = match.group(1) # e.g. LOC, MOD, SEG, PTH, ...
-                    sections.add(current_section)
+                    current_type = match.group(1) # e.g. LOC, MOD, SEG, PTH, ...
+                    types.add(current_type)
                     col_names = match.group(2).split(';') # the column names
-                    columns[current_section] = col_names
-                    data[current_section] = []
+                    columns[current_type] = col_names
+                    data[current_type] = []
             else:
                 match = re.match(r'(\w+);(.*)', line)
-                current_section = match.group(1)
-                data[current_section].append(line.split(';'))
+                current_type = match.group(1)
+                data[current_type].append(line.split(';'))
     dataframes = {section: pd.DataFrame(rows, columns=columns[section]) for section, rows in data.items() if rows}
 
     return dataframes
