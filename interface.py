e0bc77fabdcdbd7eab432c766b1b9c3b43a64e19
Simon Glomb <simon.glomb@rwth-aachen.de>
2024-06-11 02:09:08 +0200

    add a user interface

--- interface.py
+++ interface.py
@@ -1,17 +1,39 @@
+import os
+import inquirer
+
 import parse_txt
 import preprocessing
 from optimization_functions import greedy_algorithm
 import evaluation
 
+
 def main():
-    dataframes = parse_txt.parse_file('./data/inst001.txt')
-    #dataframes = parse_txt.parse_file('./data/inst002a.txt')
-    #dataframes = parse_txt.parse_file('./data/inst002b.txt')
-    #dataframes = parse_txt.parse_file('./data/inst002c.txt')
-    #dataframes = parse_txt.parse_file('./data/inst003.txt')
+    files = os.listdir("./data")
+    if not files:
+        print(f"No files found.")
+        exit(1)
+    questions = [
+        inquirer.List(
+            "file",
+            message="Select a file",
+            choices=files,
+        ),
+        inquirer.List(
+            "function",
+            message="Select an optimization strategy",
+            choices=[greedy_algorithm]
+        )
+    ]
+
+    answers = inquirer.prompt(questions)
+
+    # Print the selected file
+    selected_file = answers["file"]
+    selected_function = answers["function"]
+    dataframes = parse_txt.parse_file("./data/" + selected_file)
     result = preprocessing.convert_to_dataframe(dataframes)
     
-    mapping, path_segment_dict = greedy_algorithm.apply(result, dataframes)
+    mapping, path_segment_dict = selected_function.apply(result, dataframes)
 
     print("Costs:")
     print(evaluation.compute_costs_of_mapping(mapping, path_segment_dict, dataframes))
