8f41f7458ef04a5121b3de793b2cc532d3f5941d
Monja <hexe-monja@web.de>
2024-07-01 16:55:32 +0200

    orgastff, added random start solutions

--- monja_utility.py
+++ monja_utility.py
@@ -1,4 +1,6 @@
 from datetime import timedelta
+import random
+from monja_evaluation import compute_car_costs
 
 # find the earliest possible timeslots (and corresponding path) for a car
 def assign_timeslots(car, paths, segments, eot):
@@ -74,3 +76,98 @@ def earliest_timeslots_from_loc(car, paths, segments, start_loc, start_time, eot
     
     return departures, path, index, earliest_arrival
 
+# subroutine for local search
+# exactly as greedy, but starts with a random ordering of the cars
+def random_greedy(cars, paths, segments, eot):
+    # randomly shuffle the cars
+    car_list = [k for k in cars.keys()]
+    random.shuffle(car_list)
+    
+    # assign timeslots with earliest arrival
+    for i in range(len(car_list)):
+        id = car_list[i]
+        d, p, a = assign_timeslots(cars[id], paths, segments, eot)
+
+        # link car to chosen path, save corresponding delivery date
+        cars[id]['assignedPath'] = p
+        cars[id]['currentDelivery'] = a
+
+        # if a path was assigned, construct schedule and block capacities
+        if p != None:
+            # assign [(segID, time)] shaped schedule to car
+            schedule = []
+            path = paths[p] # path segments the car is assigned to 
+
+            for s in range(len(path)):
+                schedule.append((path[s], d[s]))
+            cars[id]['schedule'] = schedule
+
+            for j in range(len(d)):
+                segments[paths[p][j]]['timeslots'][d[j]] -= 1
+        else:
+            cars[id]['schedule'] = []
+
+    # link to each car the costs it has in the current sulution
+    for car_id in cars.keys():
+        car_cost = compute_car_costs(cars[car_id]['avlDate'], cars[car_id]['dueDate'], cars[car_id]['currentDelivery'], cars[car_id]['deliveryRef'])
+        cars[car_id]['inducedCosts'] = car_cost
+    
+    return cars, segments
+
+# assign a random, valid schedule to a car
+def assign_random_timeslots(car, paths, segments, eot):
+    # pick a random path
+    path = random.choice(car['paths'])
+    earliest_start = car['avlDate']
+    timetable = []
+    for j in range(len(paths[path])):
+        potential_times = [s for s in segments[paths[path][j]]['timeslots'].keys() if s >= earliest_start and segments[paths[path][j]]['timeslots'][s] > 0] # timeslots after the ealiest possible departure with open capacities
+        if len(potential_times) > 0:
+            departure = random.choice(potential_times)
+        else: # path is blocked -> bad luck :(
+            break
+        timetable.append(departure)
+        if j != len(paths[path])-1: # determine ealiest departure from current location
+            earliest_start = (departure + timedelta(hours=segments[paths[path][j]]['duration']+24)).replace(hour=0, minute=0, second=0, microsecond=0)
+        else: # last segment: determine arrival time
+            arrival = departure + timedelta(hours=segments[paths[path][j]]['duration'])
+          
+    if len(timetable) < len(paths[path]): # path was blocked, assign no path
+        return [], None, eot
+    
+    return timetable, path, arrival
+
+# random start solution, less dense than greedy
+def random_solution(cars, paths, segments, eot):
+    # randomly shuffle the cars
+    car_list = [k for k in cars.keys()]
+    random.shuffle(car_list)
+    for i in range(len(car_list)):
+            id = car_list[i]
+            d, p, a = assign_random_timeslots(cars[id], paths, segments, eot)
+
+            # link car to chosen path, save corresponding delivery date
+            cars[id]['assignedPath'] = p
+            cars[id]['currentDelivery'] = a
+
+            # if a path was assigned, construct schedule and block capacities
+            if p != None:
+                # assign [(segID, time)] shaped schedule to car
+                schedule = []
+                path = paths[p] # path segments the car is assigned to 
+
+                for s in range(len(path)):
+                    schedule.append((path[s], d[s]))
+                cars[id]['schedule'] = schedule
+
+                for j in range(len(d)):
+                    segments[paths[p][j]]['timeslots'][d[j]] -= 1
+            else:
+                cars[id]['schedule'] = []
+
+    # link to each car the costs it has in the current sulution
+    for car_id in cars.keys():
+        car_cost = compute_car_costs(cars[car_id]['avlDate'], cars[car_id]['dueDate'], cars[car_id]['currentDelivery'], cars[car_id]['deliveryRef'])
+        cars[car_id]['inducedCosts'] = car_cost
+        
+    return cars, segments
